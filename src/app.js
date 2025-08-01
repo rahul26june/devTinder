05091bf9690bfa071b7c2ae8b96a2e169fda93b4
Rahul Kumar <rahul.26june@gmail.com>
2025-08-01 15:45:09 +0530

    added routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.get("/users", (req, res) => {
+  res.send({firstname: "Sahej", lastname: "Singh"});
+});
+
+app.post("/users", (req, res) => {
+  res.send("This is a post request");
+});
+
+app.delete("/users", (req, res) => {
+  res.send("Sucessfully deleted user");
+});
+
 app.use("/test",(req,res)=>{
   res.send('Hello, World!');
 });
